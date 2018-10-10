772a9907f0cc30a5c9ac074873e4e7ecea2ac258
System Administrator <root@MacBook-Pro-de-mac.local>
2018-10-10 20:50:51 +0100

    MultiScreen App

---
+++ MultiScreens/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  MultiScreens
+//
+//  Created by mac on 10/8/18.
+//  Copyright © 2018 Brahim Bouhouche. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Second View"
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
